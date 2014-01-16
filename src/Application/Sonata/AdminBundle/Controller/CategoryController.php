618fbe74f493297b9c75782a69162d5a6aa285e1
Suresh <suresh@como-HP-dx2080-MT-KS826PA.(none)>
2014-01-16 12:24:21 +0530

    Added web directory bundles

---
+++ src/Application/Sonata/AdminBundle/Controller/CategoryController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Application\Sonata\AdminBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+/**
+ * Description of CategoryController
+ *
+ * @author suresh
+ */
+class CategoryController extends Controller {
+    //put your code here
+}
+
+?>
